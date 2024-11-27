897f2ebd38694540ba03a69643070442e4a5e471
Casti28 <lorenzo.castiello04@gmail.com>
2024-11-27 09:12:34 +0100

    Progetto aggiunto

---
+++ CastielloLorenzoCoolshopTest/CastielloLorenzoCoolshopTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CastielloLorenzoCoolshopTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
